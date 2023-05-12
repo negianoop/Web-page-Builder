d25b124d54d0ebc24f29b5ab8046ec6e5e7b78eb
Anoop <84010126+negianoop@users.noreply.github.com>
2023-05-12 13:47:36 +0530

    Update editor.js

--- src/js/editor.js
+++ src/js/editor.js
@@ -89,7 +89,7 @@ var editor  = grapesjs.init({
           'border',
           'box-shadow',
           'background-color', // { id: 'background-bg', property: 'background', type: 'bg' 
-          'background',
+          
           }
         ],
       },
